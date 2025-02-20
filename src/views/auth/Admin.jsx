ec50f2a20904a0b98230c69b6d17abe1d6e8fb53
wisdom michael <ijarwisdom@gmail.com>
2025-02-20 14:14:45 +0100

    code optimized

--- src/views/auth/Admin.jsx
+++ src/views/auth/Admin.jsx
@@ -29,29 +29,43 @@ const AdminLogin = () => {
 
 
     return (
-        <div className="min-w-screen min-h-screen bg-[#cdcae9] flex items-center justify-center">
-           <div className="w-[350px] text-white p-2">
-               <div className="bg-[#4a47a3] p-4 rounded-lg">
-                <h1 className="text-xl mb-3 font-bold text-center">Welcome To Wizkicks</h1>
-                <p className="text-sm mb-3 font-medium">plese Login your account</p>
+        <div className="min-w-screen min-h-screen bg-[#ffffff] flex items-center justify-center">
+           <div className="w-full min-h-screen text-black p-2 flex items-center justify-center gap-10 m-auto">
+            <div className="w-[45%] text-white bg-gradient-to-b from-[#ff8036] to-[#ff9f6b] rounded-4xl">
+                <div>
+                    <h1 className="text-5xl font-bold pl-10">Welcome <br />Admin</h1>
+                    <p>please enter your details to continue</p>
+                </div>
+            <div className="ml-20">
+                <img className="w-[500px]" src="/src/assets/Screenshot_2025-02-20_125342-removebg-preview.png" alt="" />
+            </div>
+            </div>
+               <div className="bg-[#ffffff] p-4 w-[45%]">
+                <div className="flex justify-center mb-3">
+                    <img className="w-15 " src="/src/assets/REX4-removebg-preview.png" alt="" />
+                </div>
+                <h1 className="text-2xl mb-4 font-bold text-center">Welcome To Wizkicks</h1>
+                <p className="text-sm mb-3 font-medium text-center">plese Login your account</p>
 
                 <form onSubmit={submit} className="space-y-2">
 
-                    <div className="mb-4 flex flex-col w-full gap-1">
-                        <label htmlFor="email" className="block text-sm font-medium">Email</label>
-                        <input onChange={inputHandle} value={state.email} type="email" name="email" placeholder="Email" id="email" required className="w-full px-3 py-2 rounded-md outline-none border border-slate-700 bg-transparent text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" />
+                        <label htmlFor="email" className="block text-sm font-medium pl-14">Email</label>
+                    <div className="mb-5 flex flex-col w-full justify-center items-center gap-1">
+                        <input onChange={inputHandle} value={state.email} type="email" name="email" placeholder="Email" id="email" required className=" px-3 py-3 w-[85%] rounded-xl bg-[#edeef0] text-gray-700 placeholder-gray-400 " />
                         {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
                     </div>
 
-                    <div className="mb-4 flex flex-col w-full gap-1">
-                        <label htmlFor="password" className="block text-sm font-medium">Password</label>
-                        <input onChange={inputHandle} value={state.password} type="password" name="password" placeholder="Password" id="password" required className="w-full px-3 py-2 rounded-md outline-none border border-slate-700 bg-transparent text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" />
+                        <label htmlFor="password" className="block text-sm font-medium pl-14">Password</label>
+                    <div className="mb-5 flex flex-col w-full justify-center items-center gap-1">
+                        <input onChange={inputHandle} value={state.password} type="password" name="password" placeholder="Password" id="password" required className="w-[85%] px-3 py-3 rounded-xl outline-none bg-[#edeef0] text-gray-700 placeholder-gray-400 " />
                         {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
                     </div>
 
-                    <button className=" bg-slate-800 w-full text-center hover:bg-slate-900 hover:shadow-blue-300/50 hover:shadow-lg text-white- rounded-md px-7 py-2 mb-3">
+                    <div className="flex justify-center items-center w-full">
+                    <button className=" bg-[#ff8036] flex justify-center items-center w-[85%] font-bold text-white hover:bg-slate-900 hover:shadow-blue-300/50 hover:shadow-lg text-white- rounded-xl px-7 py-3 mb-3">
                         Login
                     </button>
+                    </div>
                     <p className="text-sm text-center"> 
                         Don&apos;t have an account?                 <Link to="/register" className="text-blue-500"> 
                          Sign Up 
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -8,81 +8,108 @@ import { useState } from "react";
 // Import icons
 
 const Login = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
 
-    const[state, setState] = useState({
-        email: "",
-        password: ""
+  const inputHandle = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
     });
+  };
 
-    const inputHandle = (e) => {
-        const {name, value} = e.target;
-        setState({
-            ...state,
-            [name]: value
-        });
-    }
-    
-    const submit = (e) => {
-        e.preventDefault();
-        console.log(state);
-    }
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
 
+  return (
+    <div className="min-w-screen min-h-screen bg-[#cdcae9] flex items-center justify-center">
+      <div className="w-[350px] text-white p-2">
+        <div className="bg-[#4a47a3] p-4 rounded-lg">
+          <h1 className="text-xl mb-3 font-bold text-center">
+            Welcome To Wizkicks
+          </h1>
+          <p className="text-sm mb-3 font-medium">plese Login your account</p>
 
-    return (
-        <div className="min-w-screen min-h-screen bg-[#cdcae9] flex items-center justify-center">
-           <div className="w-[350px] text-white p-2">
-               <div className="bg-[#4a47a3] p-4 rounded-lg">
-                <h1 className="text-xl mb-3 font-bold text-center">Welcome To Wizkicks</h1>
-                <p className="text-sm mb-3 font-medium">plese Login your account</p>
+          <form onSubmit={submit} className="space-y-2">
+            <label htmlFor="email" className="block text-sm font-medium pl-14">
+              Email
+            </label>
+            <div className="mb-5 flex flex-col w-full justify-center items-center gap-1">
+              <input
+                onChange={inputHandle}
+                value={state.email}
+                type="email"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+                className=" px-3 py-3 w-[85%] rounded-xl bg-[#edeef0] text-gray-700 placeholder-gray-400 "
+              />
+              {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
+            </div>
 
-                <form onSubmit={submit} className="space-y-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium pl-14"
+            >
+              Password
+            </label>
+            <div className="mb-5 flex flex-col w-full justify-center items-center gap-1">
+              <input
+                onChange={inputHandle}
+                value={state.password}
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="password"
+                required
+                className="w-[85%] px-3 py-3 rounded-xl outline-none bg-[#edeef0] text-gray-700 placeholder-gray-400 "
+              />
+              {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
+            </div>
 
-                    <div className="mb-4 flex flex-col w-full gap-1">
-                        <label htmlFor="email" className="block text-sm font-medium">Email</label>
-                        <input onChange={inputHandle} value={state.email} type="email" name="email" placeholder="Email" id="email" required className="w-full px-3 py-2 rounded-md outline-none border border-slate-700 bg-transparent text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" />
-                        {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
-                    </div>
+            <div className="flex justify-center items-center w-full">
+              <button className=" bg-[#ff8036] flex justify-center items-center w-[85%] font-bold text-white hover:bg-slate-900 hover:shadow-blue-300/50 hover:shadow-lg text-white- rounded-xl px-7 py-3 mb-3">
+                Login
+              </button>
+            </div>
+            <p className="text-sm text-center">
+              Don&apos;t have an account?{" "}
+              <Link to="/register" className="text-blue-500">
+                Sign Up
+              </Link>
+            </p>
 
-                    <div className="mb-4 flex flex-col w-full gap-1">
-                        <label htmlFor="password" className="block text-sm font-medium">Password</label>
-                        <input onChange={inputHandle} value={state.password} type="password" name="password" placeholder="Password" id="password" required className="w-full px-3 py-2 rounded-md outline-none border border-slate-700 bg-transparent text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" />
-                        {/* <p className="text-xs text-red-600">Please enter a valid email address.</p> */}
-                    </div>
-
-                    <button className=" bg-slate-800 w-full text-center hover:bg-slate-900 hover:shadow-blue-300/50 hover:shadow-lg text-white- rounded-md px-7 py-2 mb-3">
-                        Login
-                    </button>
-                    <p className="text-sm text-center"> 
-                        Don&apos;t have an account?                 <Link to="/register" className="text-blue-500"> 
-                         Sign Up 
-                        </Link>
-                    </p>
-
-                    <div className="flex items-center justify-center gap-2 my-2">
-                        <hr className="w-1/4 border border-slate-700" />
-                        <p className="text-[12px] text-center">Or Login With</p>
-                        <hr className="w-1/4 border border-slate-700" />
-
-                    </div>
-                    <div className="flex justify-center gap-3">
-                        <div className="bg-[#db4437] w-[125px] h-[35px] rounded-full flex items-center justify-center shadow-lg hover:shadow-orange-700/50 cursor-pointer overflow-hidden">
-                            <span>
-                               <img className="w-4 h-4" src="src/assets/icons8-google.svg" alt="google logo" />
-                            </span>
-                        </div>
-                        <div className="bg-blue-600 w-[125px] h-[35px] rounded-full flex items-center justify-center shadow-lg hover:shadow-blue-500/50 cursor-pointer overflow-hidden">
-                            <span>
-                               <img className="w-5 h-5" src="src/assets/icons8-facebook (1).svg" alt="facebook logo" />
-                            </span>
-                        </div>
-
-                    </div>
-                </form>           
-     
-               </div>
-           </div>
+            <div className="flex justify-center gap-3">
+              <div className="bg-[#db4437] w-[125px] h-[35px] rounded-full flex items-center justify-center shadow-lg hover:shadow-orange-700/50 cursor-pointer overflow-hidden">
+                <span>
+                  <img
+                    className="w-4 h-4"
+                    src="src/assets/icons8-google.svg"
+                    alt="google logo"
+                  />
+                </span>
+              </div>
+              <div className="bg-blue-600 w-[125px] h-[35px] rounded-full flex items-center justify-center shadow-lg hover:shadow-blue-500/50 cursor-pointer overflow-hidden">
+                <span>
+                  <img
+                    className="w-5 h-5"
+                    src="src/assets/icons8-facebook (1).svg"
+                    alt="facebook logo"
+                  />
+                </span>
+              </div>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
